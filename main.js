38c72624443c7bae5463f0f9b83a64d37ba94994
Atti-Deutsch <send.atti.mail@gmail.com>
2021-06-01 21:22:37 +0200

    finished project

--- main.js
+++ main.js
@@ -221,3 +221,162 @@ function greet(greeting = 'Hello', name) {
 
 const greet = (greeting = 'Hello', name = 'There') => `${greeting} ${name}`;
 console.log(greet('Hi'));
+
+
+// OBJECT ORIENTED PROGRAMMING - concepts of classes and objects, reusable pieces of code 
+
+//CONSTRUCTOR FUNCTION - constructors are object we create in contrast to "built in" constructors Array and Object.
+function Person(firstName, lastName, dob) {
+    // Set object properties   // this keyword refers to the object it belongs to
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob); // Set actual date object using date
+}
+        //Example using constructor get Birthyear of dob property
+        this.getBirthYear = function() {
+            return this.dob.getFullYear();
+        }
+        //Example using constructor get fullname using templatestring
+        this.getFullName = function() {
+            return `${this.firstName} ${this.lastName}`;
+        }
+
+    // Using PROTOTYPE - 
+        
+        //Get Birth Year
+        Person.prototype.getBirthYear = function() {
+            return this.dob.getFullYear;
+        }
+
+        // Get Full Name - template string
+        Person.prototype.getFullName = function() {
+            return `${this.firstName} ${this.lastName}`;
+        }
+
+//Instantiate an object from the class meaning creating a new object from class
+    const person1 = new Person('John', 'Doe', '80-08-01');
+    const person2 = new Person('Brian', 'Liberac', '72-11-15');
+
+    console.log(person2);
+    // console.log(person2.getBirthYear());
+    // console.log(person2.getFullName());
+
+
+// BUILT IN CONSTURCTORS - arrays and objects
+const name = new String('Anders');
+console.log(typeof name);  // Shows 'object'
+const num = new Number(5);
+console.log(typeof num); // Shows 'object'
+
+
+// ES6 CLASSES 
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
+
+    // Get Birth Year
+    getBirthYear() {
+        return this.dob.getFullYear();
+    }
+
+    // Get Full Name
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+const person1 = new Person('John', 'Doe', '80-08-01');
+console.log(person1.getBirthYear());
+
+
+// ELEMENT SELECTORS
+
+    // SINGLE ELEMENT SELECTORS
+    console.log(document.getElementById('my-form'));
+    console.log(document.querySelector('container'));
+
+    // MULTIPLE ELEMENT SELECTORS
+    console.log(document.querySelectorAll('.item'));
+    console.log(document.getElementsByTagName('li'));
+    console.log(document.getElementsByClassName('item'));
+
+    const items = document.querySelectorAll('.item');
+    items.forEach((item) => console.log(item));
+
+
+// MANIPULATING THE DOM
+
+const ul = document.querySelector('.items');
+    // ul.remove();
+    // ul.lastElementChild.Remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+const btn = document.querySelector('.btn');
+// btn.style,background = 'red';
+
+
+// EVENTS
+
+// MOUSE EVENT
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    console.log(e.target.className);
+    document.getElementById('my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
+});
+
+// Keyboard Event
+const nameInput = document.querySelector('#name');
+nameInput.addEventListener('input', e => {
+  document.querySelector('.container').append(nameInput.value);
+});
+
+
+// USER FORM SCRIPT
+
+// Put DOM elements into variables
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  
+  if(nameInput.value === '' || emailInput.value === '') {
+    // alert('Please enter all fields');
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    // Remove error after 3 seconds
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    // Create new list item with user
+    const li = document.createElement('li');
+
+    // Add text node with input values
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+
+    // Add HTML
+    // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+
+    // Append to ul
+    userList.appendChild(li);
+
+    // Clear fields
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
+
+
